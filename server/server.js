8efa6e0f0058b7bb1be474a684612b6a5c87dddc
Jasper Barcial <Jbarcial25@gmail.com>
2022-06-21 07:22:07 -0700

    deploying again

--- server/server.js
+++ server/server.js
@@ -28,6 +28,7 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
+//something
 db.once('open', () => {
   app.listen(PORT, () => {
     console.log(`API server running on port ${PORT}!`);
